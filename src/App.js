d8d5dca743b027ce5dea01fe40ed2a91fd3c84c5
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2023-06-06 17:59:54 +0300

    another commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React, {useState} from 'react';
 import Editor from './Editor'
 import Previewer from './Previewer';
+
 import './App.css';
 
 function App() {
 
-  const [markdown, setMarkdown] = useState('');
+  
+
+  
+
+  const [markdown, setMarkdown] = useState(`
+
+  
+  # Heading 
+  ## Heading 2
+  [Link](https://example.com)
+  \`Inline Code\`
+  
+  \`\`\`
+  // Code Block
+  const example = 'Hello, world!';
+  \`\`\`
+  
+  - List Item 1
+  - List Item 2
+  
+  > Blockquote
+  
+  ![Image](https://example.com/image.jpg)
+  
+  **Bold Text**
+  
+  # Heading\n\nParagraph with line break\n\n- List item 1\n- List item 2
+  `);
+  
+
+  
+  
+
 
   const handleChange = (e) => {
     setMarkdown(e.target.value);
@@ -15,11 +48,13 @@ function App() {
 
   return (
     <div className="App">
+      <div className="preview-editor-container" >
 
-      <Editor markdown={markdown} onChange={handleChange} />
-      <Previewer markdown={markdown} />
+        <Editor markdown={markdown} onChange={handleChange} />
+        <Previewer markdown={markdown} />
       
-    </div>
+    </div>   
+  </div>
   );
 }
 
--- src/Previewer.js
+++ src/Previewer.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import breaks from 'remark-breaks';
+
+
+const Previewer = ({ markdown }) => {
+  const options = {
+    remarkPlugins:[breaks],
+  }
+  
+
+  return (
+    <div id="preview" className="preview-box">
+      <ReactMarkdown children={markdown} {...options}/>
+    </div>
+  );
+};
+
+export default Previewer;
+
+
+
+
+
+/*
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Renderer } from 'marked';
 
 const Previewer = ({markdown}) => {
+
+    
+
+    const renderer = new Renderer();
+
+    renderer.paragraph = (text) => {
+        return `<p>${text.replace(/\n/g, '<br>')}</p>`;
+    };
+
+    const options = {
+       renderer: renderer,
+       breaks: true,
+    };
+
+
+
+
+
     return (
         <div id="preview">
-            <ReactMarkdown children={markdown} />
+            <ReactMarkdown children={markdown} {...options}/>
         </div>
     );
 }
 
-export default Previewer
+export default Previewer;
+
+*/
